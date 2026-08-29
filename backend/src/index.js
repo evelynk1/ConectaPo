8ddd5c2c67988e87f534bfba6c76cf205a18725b
PabloRodriguez <ptrodriguez@gmail.com>
2026-08-29 13:18:18 -0400

    crear estructura inicial y servidor express (CON-67)

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'; // Carga las variables del archivo .env automáticamente
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// ==========================================
+// MIDDLEWARES GLOBALES
+// ==========================================
+// Permite que tu frontend se comunique con este backend sin bloqueos de seguridad
+app.use(cors());
+// Permite que el backend entienda los datos que lleguen en formato JSON
+app.use(express.json());
+
+// ==========================================
+// RUTA DE PRUEBA
+// ==========================================
+app.get('/', (req, res) => {
+    res.json({
+        mensaje: '¡Servidor de ConectaPo funcionando al 100%!',
+        estado: 'Online'
+    });
+});
+
+// ==========================================
+// INICIO DEL SERVIDOR
+// ==========================================
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
